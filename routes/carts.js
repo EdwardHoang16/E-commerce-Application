109241a8abb78039437d6ef0edfca63422ea8804
Edward Hoang <edward.hoang@sjsu.edu>
2020-11-18 11:21:59 -0800

    Add functionality to add to cart and delete from cart + styling

--- routes/carts.js
+++ routes/carts.js
@@ -1,15 +1,61 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
 // Receive a post request to add an item to the cart
-router.post("/cart/products", (req, res) => {
-    console.log(req.body.productId);
-    res.send("Product added to cart");
+router.post("/cart/products", async (req, res) => {
+    let cart;
+    if (!req.session.cartId) {
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.items.push({ id: req.body.productId, quantity: 1 });
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
+
+    res.redirect("/cart");
 })
 
-// Receive a get request to see all items in cart
+// Receive a GET request to show all items in cart
+router.get("/cart", async (req, res) => {
+    if (!req.session.cartId) {
+        res.redirect("/");
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
+
 
 //Receive a post to delete an item in the cart
+router.post("/cart/products/delete", async (req, res) => {
+    const { itemId } = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter(item => item.id !== itemId);
+
+    await cartsRepo.update(req.session.cartId, { items });
+
+    res.redirect("/cart");
+});
 
 module.exports = router;
